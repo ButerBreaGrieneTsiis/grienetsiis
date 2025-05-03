c11bfc3c4e44cb48e630f4edcacef43e1fd42810
TomScheeper <tomscheeper@gmail.com>
2025-05-03 22:32:31 +0200

    toon bedrag

--- src/grienetsiis/__init__.py
+++ src/grienetsiis/__init__.py
@@ -1,4 +1,4 @@
-from .gereedschap import jaar_maand_iterator
+from .gereedschap import jaar_maand_iterator, toon_bedrag
 from .lezerschrijver import open_json, opslaan_json
 from .invoer import invoer_validatie, invoer_kiezen
 from .kleuren import Kleur, kleurschaal, kleurtinten, RGBCMY
--- src/grienetsiis/gereedschap.py
+++ src/grienetsiis/gereedschap.py
@@ -14,3 +14,38 @@ def jaar_maand_iterator(
     for jaar_maand in range(jaar_maand_start, jaar_maand_eind + 1):
         jaar, maand = divmod(jaar_maand, 12)
         yield jaar, maand+1
+
+def decimaal_getal(
+    getal           :   float,
+    groeperen_per   :   int = 3,
+    absolute        :   bool = False,
+    ) -> str:
+    
+    getal_tekst = str(abs(getal))
+    
+    integer = getal_tekst.split(".")[0]
+    decimalen = getal_tekst.split(".")[1]
+    
+    integer_geformatteerd = u"\u2009".join([integer[::-1][index:index+groeperen_per] for index in range(0, len(integer), groeperen_per)])[::-1]
+    decimalen_geformatteerd = u"\u2009".join([decimalen[index:index+groeperen_per] for index in range(0, len(decimalen), groeperen_per)])
+    
+    teken = "-" if (getal < 0 and not absolute) else ""
+    
+    return f"{teken}{integer_geformatteerd},{decimalen_geformatteerd}"
+
+def toon_bedrag(
+    bedrag          :   float,
+    symbool         :   str     =   u"\u20ac",
+    symbool_ervoor  :   bool    =   True,
+    spatie          :   bool    =   True,
+    ) -> str:
+    
+    bedrag_geformateerd = decimaal_getal(bedrag, absolute = True)
+    
+    bedrag_tekst    =   bedrag_geformateerd.split(",")[0]+",- " if abs(bedrag) % 1 == 0 else bedrag_geformateerd
+    bedrag_teken    =   "" if bedrag > 0 else "-"
+    
+    if symbool_ervoor:
+        return f"{bedrag_teken}{symbool}{" "*spatie}{bedrag_tekst}"
+    else:
+        return f"{bedrag_teken}{bedrag}{" "*spatie}{bedrag_tekst}"
