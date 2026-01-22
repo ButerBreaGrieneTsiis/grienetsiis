e364afff20b0736c0c260fc0772d5a58799a604f
TomScheeper <tomscheeper@gmail.com>
2026-01-22 22:52:28 +0100

    update.

--- src/grienetsiis/opdrachtprompt/__init__.py
+++ src/grienetsiis/opdrachtprompt/__init__.py
@@ -2,9 +2,11 @@
 grienetsiis.opdrachtprompt
 """
 from grienetsiis.opdrachtprompt.invoer import invoeren, kiezen
+from grienetsiis.opdrachtprompt.menu import Menu
 
 
 __all__ = [
     "invoeren",
     "kiezen",
+    "Menu",
     ]
