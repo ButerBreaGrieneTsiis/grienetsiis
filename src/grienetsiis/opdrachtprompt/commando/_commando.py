3524780729f003093bddf689d99cdfc6cd913723
TomScheeper <tomscheeper@gmail.com>
2026-02-11 21:53:22 +0100

    commando betere repr

--- src/grienetsiis/opdrachtprompt/commando/_commando.py
+++ src/grienetsiis/opdrachtprompt/commando/_commando.py
@@ -3,5 +3,5 @@ from grienetsiis.types import Singleton
 
 class Commando(metaclass = Singleton):
     def __call__(self, *args, **kwargs) -> RuntimeError: raise RuntimeError("Commando kan niet uitgevoerd worden.")
-    def __repr__(self) -> str: return self.__class__.__name__.upper()
-    def __str__(self) -> str: return self.__class__.__name__.upper()
+    def __repr__(self) -> str: return self.__class__.__name__.replace("_", "").upper()
+    def __str__(self) -> str: return self.__class__.__name__.replace("_", "").upper()
